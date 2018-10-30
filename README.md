04d7a208a0f3a7d896447dc735ffe27cfacf5373
Rahul <shenoyrahul444@gmail.com>
2018-10-29 21:40:05 -0700

    Part-1

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # infinity-and-beyond
+
+###Part1:
+    Create virtual environment:
+    
+    1> python3 -m venv venv
+    2> source venv/bin/activate
+
+    Installing Flask:
+    > pip install flask
+    
+    Installed python-dotenv to include all environment variables requuired to start Flask
+    
+ 
--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    return 'To Infinity and Beyond!'
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True,use_debugger=False,use_reloader=True,host='0.0.0.0',passthrough_errors=True)
+
